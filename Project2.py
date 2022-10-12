0732bc13b6c148a1362f0f1532bdf46d1a86979b
Jonatch <115596067+Jonatch@users.noreply.github.com>
2022-10-11 21:02:41 -0400

    stubs

--- Project2.py
+++ Project2.py
@@ -1,2 +1,17 @@
 # Main Class
 # Zach, Tyler, Sean, Jonah
+
+def add_user():
+    pass
+
+def remove_user():
+    pass
+
+def check_password():
+    pass
+
+def print_file():
+    pass
+
+def save_to_file():
+    pass 

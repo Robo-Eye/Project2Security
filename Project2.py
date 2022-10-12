39fe5bf1ee5f78ad38cec3f410f0bdc496f5ab20
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2022-10-11 20:54:45 -0400

    Comments In Main

--- Project2.py
+++ Project2.py
@@ -1,2 +1,14 @@
 # Main Class
 # Zach, Tyler, Sean, Jonah
+
+# Task 1: The project allows the application to add a new user to the password store.
+
+# Task 2: The project allows the application to remove a user from the password store.
+
+# Task 3: The project allows the application to check a user’s password.
+
+# Task 4: The project allows the application to print the password file.
+
+# Task 5: The application correctly saves and loads the password store to/from a file.
+
+# Task 6: Thoroughly document your code.
